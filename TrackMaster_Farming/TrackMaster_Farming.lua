9ff927f89c974d0c286d2c548abb8b0236c2c899
James Parker <jamez226@hotmail.com>
2014-05-11 21:51:35 +0100

    Removed filter for Relic Hunters which was accidentally commited

--- TrackMaster_Farming/TrackMaster_Farming.lua
+++ TrackMaster_Farming/TrackMaster_Farming.lua
@@ -130,7 +130,7 @@ function TrackMaster_Farming:CalculateDistance(vector)
 end
 
 function TrackMaster_Farming:OnUnitCreated(unit)
-	if unit:GetType() == 'Harvest' and unit:CanBeHarvestedBy(GameLib.GetPlayerUnit()) and unit:GetHarvestRequiredTradeskillName() == "Relic Hunter" then
+	if unit:GetType() == 'Harvest' and unit:CanBeHarvestedBy(GameLib.GetPlayerUnit()) then
 		self.units[unit:GetId()] = unit
 	end
 end
